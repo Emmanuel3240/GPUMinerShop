be0487914506ebe0a3c98290599ecf9dbfffd6b2
Emmanuel Lagneau <emmanwel@gmail.com>
2021-06-08 22:56:02 -0300

    navbar ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Button from '@material-ui/core/Button'
+
+import Typography from "@material-ui/core/Typography";
+
+import {ThemeProvider} from '@material-ui/styles'
+import theme from './temaConfig' 
+
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div>
-prueba
-    </div>
+  <NavBar/>
   );
 }
 
