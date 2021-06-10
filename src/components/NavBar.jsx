73fdefc8cf169be6dd06949c2fe4c30289c42cf8
Emmanuel Lagneau <emmanwel@gmail.com>
2021-06-10 16:48:26 -0300

    Navbar completo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles, IconButton, Button } from "@material-ui/core";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, IconButton, Button} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import PersonIcon from '@material-ui/icons/Person';
+
 
 const useStyle = makeStyles((theme) => ({
     offset: {
@@ -24,14 +27,25 @@ const NavBar = () =>{
     <React.Fragment>
     <AppBar position="fixed" color="primary">
       <Toolbar>
-          <IconButton edge="start" className = {classes.menuButton} color="inherit" aria-lable="menu">
+          <IconButton edge="start" className = {classes.menuButton} color="inherit">
             <MenuIcon/>
           </IconButton>
+          <Button variant="text" color="inherit">
+          Home
+        </Button>
+        <Button variant="text" color="inherit">
+          Store
+        </Button>
+        <Button variant="text" color="inherit">
+          About
+        </Button>
         <Typography align="center" variant="h6" className={classes.title} >
           GPUMinerShop
         </Typography>
-
-        <Button variant="text" color="inherit">
+        <Button variant="text" color="inherit" endIcon={<ShoppingCartIcon/>}>
+          Cart
+        </Button>
+        <Button variant="text" color="inherit" endIcon={<PersonIcon/>}>
           Login
         </Button>
       </Toolbar>
