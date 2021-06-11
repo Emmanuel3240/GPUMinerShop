1a39a69641f54f3e3fb9cb423d343dee03f7c152
Emmanuel Lagneau <emmanwel@gmail.com>
2021-06-10 21:36:27 -0300

    CartWidget funcionando OK

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,10 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, IconButton, Button} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
 import PersonIcon from '@material-ui/icons/Person';
 import {NavBarStyles} from './Styles/NavBarStyles';
+import Badge from './CartWidget'
 
 const useStyles = makeStyles((theme) => NavBarStyles(theme));
 
@@ -31,9 +32,9 @@ const NavBar = () =>{
         <Typography align="center" variant="h6" className={classes.title} >
           GPUMinerShop
         </Typography>
-        <Button variant="text" color="inherit" endIcon={<ShoppingCartIcon/>}>
-          Cart
-        </Button>
+        <IconButton aria-label="show cart items" color="inherit">
+          <Badge/>
+        </IconButton>
         <Button variant="text" color="inherit" endIcon={<PersonIcon/>}>
           Login
         </Button>
